c70723c79a1d0e374f3740893906913fae8b99fa
Heikki Parkkonen <heikki@hexagon.fi>
2020-05-15 21:40:00 +0300

    ooh node package compatibility

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 // maybe™ 2020 Connor Intergalactic
 
 this.__defineGetter__('maybe', () => Math.random() >= 0.5);
+
+if (typeof module !== 'undefined' && module.exports) {
+	module.exports = () => Math.random() >= 0.5;
+}

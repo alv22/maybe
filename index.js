6adda118c6c50cc814f7bca8a69ba01dc5f31083
Heikki Parkkonen <heikki@hexagon.fi>
2021-08-17 16:27:34 +0300

    nope

--- index.js
+++ index.js
@@ -1,8 +1,4 @@
 // maybe™ 2020 Connor Intergalactic
 
-const maybe = () => Math.random() >= 0.5;
-
 // this = window (browser) || global (node)
-this.__defineGetter__('maybe', maybe());
-
-export default maybe;
+this.__defineGetter__('maybe', Math.random() >= 0.5);
